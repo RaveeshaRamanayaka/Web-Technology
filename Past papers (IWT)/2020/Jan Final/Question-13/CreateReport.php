82bb0063b22726167a3cc5f1ce2b7f910e2c6427
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-11-14 23:28:27 +0530

    Update CreateReport.php

--- Past papers (IWT)/2020/Jan Final/Question-13/CreateReport.php
+++ Past papers (IWT)/2020/Jan Final/Question-13/CreateReport.php
@@ -1,1 +1,41 @@
+<?php
+$connection = new mysqli("localhost", "root", "user321", "employee");
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Report Sheet</title>
+</head>
+<body>
+<div>
+    <table border="2">
+        <tr>
+            <th>Employee</th>
+            <th>Employee Name</th>
+            <th>Basic Salary</th>
+            <th>OT Rate</th>
+            <th>OT Hours</th>
+            <th>Salary With OT</th>
+        </tr>
+        <?php
+        $sqlQuery = "SELECT empID,empName,BasicSalary,OTRate,OTHours,(BasicSalary + (OTRate*OTHours)) 
+            AS 'Salary With OT' FROM salary";
+
+        $employeeLis = $connection->query($sqlQuery);
+        while ($Result = $employeeLis->fetch_assoc()) {
+            echo "<tr>" .
+                "<td>" . $Result['empID'] . "</td>" .
+                "<td>" . $Result['empName'] . "</td>" .
+                "<td>" . $Result['BasicSalary'] . "</td>" .
+                "<td>" . $Result['OTRate'] . "</td>" .
+                "<td>" . $Result['OTHours'] . "</td>" .
+                "<td>" . $Result['Salary With OT'] . "</td>" .
+                "</tr>";
+        }
+        ?>
+    </table>
+</div>
+</body>
+</html>
